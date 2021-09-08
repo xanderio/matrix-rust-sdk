9fb5267360b7628d0fd574674cb6f3a44340638f
Damir Jelić <poljar@termina.org.uk>
2021-09-08 13:42:40 +0200

    feat(sdk): Expose the cross signing user identities
    This patch adds support to fetch cross signing user identities from the
    crypto store, those can now be used to request verifications and inspect
    the master keys of users.

--- matrix_sdk/src/lib.rs
+++ matrix_sdk/src/lib.rs
@@ -98,14 +98,13 @@ pub mod room;
 mod room_member;
 
 #[cfg(feature = "encryption")]
-mod device;
+#[cfg_attr(feature = "docs", doc(cfg(encryption)))]
+pub mod identities;
 #[cfg(feature = "encryption")]
+#[cfg_attr(feature = "docs", doc(cfg(encryption)))]
 pub mod verification;
 
 pub use client::{Client, ClientConfig, LoopCtrl, RequestConfig, SyncSettings};
-#[cfg(feature = "encryption")]
-#[cfg_attr(feature = "docs", doc(cfg(encryption)))]
-pub use device::Device;
 pub use error::{Error, HttpError, HttpResult, Result};
 pub use event_handler::{CustomEvent, EventHandler};
 pub use http_client::HttpSend;
